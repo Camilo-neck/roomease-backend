ba1c189ff70efe3d6ab474962ae629b2aa1758d7
Camilo-neck <ccuello@unal.edu.co>
2023-02-28 15:28:27 -0500

    Fix server starting

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
 import express from 'express'
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5001
 const app = express()
 
 app.get('/', (req, res) => {
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-	console.log(`Server listening on port ${port}`)
+	console.log(`Server listening on  http://localhost:${port}`)
 })
