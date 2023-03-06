e1a43a32bb48c2db1275138814a3616e25697c70
Camilo <69314816+Camilo-neck@users.noreply.github.com>
2023-03-05 20:01:47 -0500

    Update user.schema.ts
    - Set time stamps (To save createdDate and updatedDate).
    - Set select=false attribute to password, to don't retrieve it on fetch

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,8 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
-	password: { type: String, required: true },
-	createdAt: { type: Date, default: Date.now },
-});
+	password: { type: String, required: true, select: false }
+}, {timestamps: true);
 
 export default userSchema;
