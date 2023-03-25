fa62a93fb9d49ed472f490c71b09b51a5a5f4021
jorozcove <jorozcove@unal.edu.co>
2023-03-24 20:40:48 -0500

    minor changes incluiding variable or filename changes

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { JsonWebTokenError } from "jsonwebtoken";
+
+interface IPayload {
+	_id: string;
+	iat: number;
+	exp: number;
+}
+
+export const Auth = (req: Request, res: Response, next: NextFunction) => {
+	console.log("IN: AUTH MIDDLEWARE");
+	const token = req.header("auth-token");
+	if (!token) {
+		return res.status(401).json("No token provided");
+	}
+
+	try {
+		const payload = jwt.verify(
+			token,
+			process.env.TOKEN_SECRET || "S26ZNvgSv4",
+		) as IPayload;
+
+		req.userId = payload._id;
+
+		console.log("OUT: AUTH MIDDLEWARE");
+		next();
+		//catch JsonWebTokenError
+	} catch (error) {
+		if (error instanceof JsonWebTokenError) {
+			return res.status(401).json("Invalid or expired token");
+		} else {
+			return res.status(500).json("Internal server error");
+		}
+	}
+};
