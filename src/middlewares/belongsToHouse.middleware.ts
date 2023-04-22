8de300695cbaf439b6d859f0abce6ae0df2d21d7
jorozcove <jorozcove@unal.edu.co>
2023-04-22 12:53:25 -0500

    minor change on conditionals

--- src/middlewares/belongsToHouse.middleware.ts
+++ src/middlewares/belongsToHouse.middleware.ts
@@ -4,15 +4,14 @@ import houseModel from "@/db/models/house.model";
 import { STATUS_CODES } from "@/utils/constants";
 
 async function belongsToHouse(req: Request, res: Response, next: NextFunction) {
-	const { houseId } = req.params;
-	const { house_id } = req.body;
-
 	let house = undefined;
 
-	if (houseId) {
-		house = await houseModel.findOne({ _id: houseId, users: req.userId });
-	} else if (house_id) {
-		house = await houseModel.findOne({ _id: house_id, users: req.userId });
+	if (req.params.houseId) {
+		house = await houseModel.findOne({ _id: req.params.houseId, users: req.userId });
+	} else if (req.body.house_id) {
+		house = await houseModel.findOne({ _id: req.body.house_id, users: req.userId });
+	} else if (req.query.house_id) {
+		house = await houseModel.findOne({ _id: req.query.house_id, users: req.userId });
 	} else {
 		return res.status(STATUS_CODES.BAD_REQUEST).json({ message: "Invalid request, house id missing" });
 	}
