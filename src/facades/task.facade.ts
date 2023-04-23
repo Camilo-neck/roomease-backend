45dd70a7ec16a11596d880b45d2eb502913135b1
jorozcove <jorozcove@unal.edu.co>
2023-04-22 19:34:33 -0500

    refactor of tsk facade - added validation functions - changed forEach -> updateMany

--- src/facades/task.facade.ts
+++ src/facades/task.facade.ts
@@ -1,10 +1,8 @@
 import { Request, Response } from "express";
-import jwt from "jsonwebtoken";
 
 import { ServerError } from "@/errors/server.error";
 import { STATUS_CODES } from "@/utils/constants";
 
-import houseModel from "../db/models/house.model";
 import taskModel from "../db/models/task.model";
 import userModel from "../db/models/user.model";
 
@@ -25,17 +23,7 @@ class TaskFacade {
 	public async create(req: Request, res: Response): Promise<Response | undefined> {
 		const { users_id } = req.body;
 
-		//check if users exist
-		const users = await userModel.find({ _id: { $in: users_id } });
-		if (users.length !== users_id.length) {
-			throw new ServerError("Invalid users, one or more users does not exist", STATUS_CODES.BAD_REQUEST);
-		}
-
-		//check if users belong to the house
-		const house = req.house;
-		if (!users_id.every((user_id: string) => house.users.includes(user_id))) {
-			throw new ServerError("Invalid users, one or more users does not belong to the house", STATUS_CODES.BAD_REQUEST);
-		}
+		await validate_users(users_id, req.house);
 
 		const taskData = req.body;
 		taskData["created_by"] = req.userId;
@@ -44,13 +32,7 @@ class TaskFacade {
 		const task = new taskModel(taskData);
 
 		await task.save();
-
-		users.forEach((user) => {
-			user.tasks.push(task._id.toString());
-			user.save();
-		});
-
-		//userModel.updateMany({ _id: { $in: users_id } }, { $push: { tasks: task._id.toString() } });
+		await userModel.updateMany({ _id: { $in: users_id } }, { $push: { tasks: task._id.toString() } });
 
 		return res.status(STATUS_CODES.CREATED).json({
 			message: "Task created successfully",
@@ -58,21 +40,21 @@ class TaskFacade {
 		});
 	}
 	public async update(req: Request, res: Response): Promise<Response | undefined> {
-		const { id } = req.params;
+		const { task_id } = req.params;
 		const updateTask = req.body;
-		const task = await taskModel.findByIdAndUpdate(id, updateTask, { new: true });
+
+		await validate_task(task_id, req.userId);
+		await validate_users(updateTask.users_id, req.house);
+
+		const task = await taskModel.findByIdAndUpdate(task_id, updateTask, { new: true });
 
 		if (!task) {
-			return res.status(404).json({ message: "Task not found" });
+			throw new ServerError("Task not found", STATUS_CODES.BAD_REQUEST);
 		}
 
 		//Modificar lista de tareas de los usuarios
-		await userModel.updateMany({ $pull: { tasks: id } });
-		const users = await userModel.find({ _id: { $in: task.users_id } });
-		users.forEach((user) => {
-			user.tasks.push(task._id.toString());
-			user.save();
-		});
+		await userModel.updateMany({ $pull: { tasks: task_id } });
+		await userModel.updateMany({ _id: { $in: task.users_id } }, { $push: { tasks: task._id.toString() } });
 
 		return res.status(STATUS_CODES.OK).json({ message: "Task updated", task });
 	}
@@ -81,20 +63,11 @@ class TaskFacade {
 		const taskId: string = req.params.id;
 		const userId = req.userId;
 
-		const task = await taskModel.findById(taskId);
-		if (!task) {
-			return res.status(404).json({ message: "Task not found" });
-		}
+		await validate_task(taskId, userId);
 
-		const user = await userModel.findById(userId);
-		if (!user) {
-			return res.status(404).json({ message: "User not found" });
-		}
-		if (!user.tasks.includes(taskId)) {
-			return res.status(404).json({ message: "The user does not have that task." });
-		}
 		await taskModel.findByIdAndDelete(taskId);
 		await userModel.updateMany({ $pull: { tasks: taskId } });
+
 		return res.status(STATUS_CODES.OK).json({ message: "Task deleted" });
 	}
 
@@ -102,22 +75,12 @@ class TaskFacade {
 		const taskId: string = req.params.id;
 		const userId = req.userId;
 
-		const task = await taskModel.findById(taskId);
-		if (!task) {
-			return res.status(404).json({ message: "Task not found" });
-		}
+		const task = await validate_task(taskId, userId);
 
-		const user = await userModel.findById(userId);
-		if (!user) {
-			return res.status(404).json({ message: "User not found" });
-		}
-		if (!user.tasks.includes(taskId)) {
-			return res.status(404).json({ message: "The user does not have that task." });
-		}
 		task.done = !task.done;
 		task.save();
 
-		return res.status(STATUS_CODES.OK).json({ message: "Task marked as done/not done" });
+		return res.status(STATUS_CODES.OK).json({ message: `Task marked as ${task.done ? "done" : "undone"}` });
 	}
 
 	public async list(req: Request, res: Response): Promise<Response | undefined> {
@@ -126,4 +89,37 @@ class TaskFacade {
 	}
 }
 
+/////////////
+
+async function validate_users(users_id: string[], house: any = undefined): Promise<any> {
+	const users = await userModel.find({ _id: { $in: users_id } });
+	if (users.length !== users_id.length) {
+		throw new ServerError("Invalid users, one or more users does not exist", STATUS_CODES.BAD_REQUEST);
+	}
+
+	if (house) {
+		if (!users_id.every((user_id: string) => house.users.includes(user_id))) {
+			throw new ServerError("Invalid users, one or more users does not belong to the house", STATUS_CODES.BAD_REQUEST);
+		}
+	}
+
+	return users;
+}
+
+async function validate_task(task_id: string, user_id: string): Promise<any> {
+	const user = await userModel.findById(user_id);
+	if (!user) {
+		throw new ServerError("User not found", STATUS_CODES.BAD_REQUEST);
+	}
+	const task = await taskModel.findById(task_id);
+	if (!task) {
+		throw new ServerError("Task not found", STATUS_CODES.BAD_REQUEST);
+	}
+
+	if (!user?.tasks.includes(task_id)) {
+		throw new ServerError("The user does not have that task.", STATUS_CODES.BAD_REQUEST);
+	}
+
+	return task;
+}
 export default new TaskFacade();
