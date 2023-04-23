d4cf613a03445fb8a3c93d805b05390960fc7cdb
jorozcove <jorozcove@unal.edu.co>
2023-04-23 13:22:34 -0500

    bug fix with error status code

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,9 +3,8 @@ import { NextFunction, Request, Response } from "express";
 import { ServerError } from "@/errors/server.error";
 
 export default (err: ServerError, _req: Request, res: Response, _next: NextFunction) => {
-	return res
-		.json({
-			message: err.message,
-		})
-		.status(err.code);
+	console.log(err.code);
+	return res.status(err.code).json({
+		message: err.message,
+	});
 };
