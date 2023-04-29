9d7225407936ebbb3c7f28afb07d27797aba433e
jorozcove <jorozcove@unal.edu.co>
2023-04-29 13:15:34 -0500

    fixed dumb error

--- src/middlewares/schemaValidator.middeware.ts
+++ src/middlewares/schemaValidator.middeware.ts
@@ -1,11 +1,12 @@
 import { NextFunction, Request, Response } from "express";
 import { ObjectSchema } from "joi";
+import { STATUS_CODES } from "@/utils/constants";
 
 export const SchemaValidator = (schema: ObjectSchema): ((...args: any[]) => any) => {
 	return (req: Request, res: Response, next: NextFunction) => {
 		const { error } = schema.validate(req.body);
 		if (error) {
-			return res.status(400).json({
+			return res.status(STATUS_CODES.BAD_REQUEST).json({
 				message: error.details[0].message,
 			});
 		}
