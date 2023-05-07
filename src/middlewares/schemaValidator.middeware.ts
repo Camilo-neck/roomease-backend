29a8432fd13f53adf5264c0f4eac4016c8d1d19d
Jaime <jmaciass@unal.edu.co>
2023-05-07 14:21:54 -0500

    Removed ParamsValidator and QueryValidator middlewares, now SchemaValidator handles that

--- src/middlewares/schemaValidator.middeware.ts
+++ src/middlewares/schemaValidator.middeware.ts
@@ -3,16 +3,17 @@ import { ObjectSchema } from "joi";
 
 import { STATUS_CODES } from "@/utils/constants";
 
-import { STATUS_CODES } from "@/utils/constants";
+type RequestType = "body" | "query" | "params";
 
-export const SchemaValidator = (schema: ObjectSchema): ((...args: any[]) => any) => {
-	return (req: Request, res: Response, next: NextFunction) => {
-		const { error } = schema.validate(req.body);
-		if (error) {
-			return res.status(STATUS_CODES.BAD_REQUEST).json({
-				message: error.details[0].message,
-			});
-		}
-		next();
-	};
+export const SchemaValidator = (schema: ObjectSchema, type: RequestType): ((...args: any[]) => any) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const requestObject = type === "body" ? req.body : type === "query" ? req.query : req.params;
+    const { error } = schema.validate(requestObject);
+    if (error) {
+      return res.status(STATUS_CODES.BAD_REQUEST).json({
+        message: error.details[0].message,
+      });
+    }
+    next();
+  };
 };
