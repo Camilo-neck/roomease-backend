fc78993b684f9cd9cfaf88364386313b64da843d
Jaime <jmaciass@unal.edu.co>
2023-05-06 22:31:46 -0500

    Added middlewares and schemas for routes with params and queries

--- src/routes/house.routes.ts
+++ src/routes/house.routes.ts
@@ -5,27 +5,28 @@ import { Auth } from "@/middlewares/auth.middleware";
 import { BelongsToHouse } from "@/middlewares/belongsToHouse.middleware";
 import { HouseExist } from "@/middlewares/houseExists.middleware";
 import { Owner } from "@/middlewares/owner.middleware";
+import { ParamsValidator } from "@/middlewares/paramsValidator.middleware";
 import { SchemaValidator } from "@/middlewares/schemaValidator.middeware";
 import { FIELD_TYPES } from "@/utils/constants";
 
 import houseController from "../controllers/house.controller";
-import { createHouseSchema, handleJoinSchema, updateHouseSchema } from "../utils/RouteSchemas/houseRouteSchemas";
+import { createHouseSchema, handleJoinSchema, paramsHouseCode,paramsSchema,updateHouseSchema } from "../utils/RouteSchemas/houseRouteSchemas";
 
 const router = express.Router();
 
 router.get("/", Auth, asyncError(houseController.list)); //should be disabled
 router.post("/", [SchemaValidator(createHouseSchema), Auth], asyncError(houseController.create));
-router.get("/:houseId", [Auth, HouseExist(FIELD_TYPES.PARAMS), BelongsToHouse], asyncError(houseController.getById));
+router.get("/:houseId", [Auth, ParamsValidator(paramsSchema),HouseExist(FIELD_TYPES.PARAMS), BelongsToHouse], asyncError(houseController.getById));
 router.put(
 	"/:houseId",
-	[SchemaValidator(updateHouseSchema), Auth, HouseExist(FIELD_TYPES.PARAMS), Owner],
+	[ParamsValidator(paramsSchema),SchemaValidator(updateHouseSchema), Auth, HouseExist(FIELD_TYPES.PARAMS), Owner],
 	asyncError(houseController.update),
 );
-router.delete("/:houseId", [Auth, HouseExist(FIELD_TYPES.PARAMS), Owner], asyncError(houseController.delete));
-router.put("/join/:house_code", [Auth, HouseExist(FIELD_TYPES.PARAMS)], asyncError(houseController.join));
+router.delete("/:houseId", [Auth, ParamsValidator(paramsSchema), HouseExist(FIELD_TYPES.PARAMS), Owner], asyncError(houseController.delete));
+router.put("/join/:house_code", [Auth, ParamsValidator(paramsHouseCode), HouseExist(FIELD_TYPES.PARAMS)], asyncError(houseController.join));
 router.put(
 	"/handleJoin/:houseId",
-	[SchemaValidator(handleJoinSchema), Auth, HouseExist(FIELD_TYPES.PARAMS), Owner],
+	[ParamsValidator(paramsSchema), SchemaValidator(handleJoinSchema), Auth, HouseExist(FIELD_TYPES.PARAMS), Owner],
 	asyncError(houseController.handleJoin),
 );
 
