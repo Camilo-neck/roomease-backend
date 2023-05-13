05bd6589bdc24a10a4ff91d9b4cab958eeb7df3a
Jaime <jmaciass@unal.edu.co>
2023-05-13 16:56:59 -0500

    Added collection of notifications.

---
+++ src/db/models/notification.model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+import { INotification } from "@/dtos/INotification.dto";
+
+
+const Schema = mongoose.Schema;
+
+const notificationSchema = new Schema(
+	{
+		type: { type: String, required: true },
+		description: { type: String, required: true },
+    }
+);
+
+notificationSchema.post("save", async (doc) => {
+	//
+});
+
+notificationSchema.post("deleteOne", async (doc) => {
+    //
+});
+
+const notificationModel = mongoose.model<INotification>("Notification", notificationSchema);
+
+export default notificationModel;
