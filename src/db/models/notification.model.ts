a646090b8f17fd4b94084d137a244927adf4edfa
Camilo-neck <ccuello@unal.edu.co>
2023-05-15 08:41:27 -0500

    prettier

--- src/db/models/notification.model.ts
+++ src/db/models/notification.model.ts
@@ -2,22 +2,19 @@ import mongoose from "mongoose";
 
 import { INotification } from "@/dtos/INotification.dto";
 
-
 const Schema = mongoose.Schema;
 
-const notificationSchema = new Schema(
-	{
-		type: { type: String, required: true },
-		description: { type: String, required: true },
-    }
-);
+const notificationSchema = new Schema({
+	type: { type: String, required: true },
+	description: { type: String, required: true },
+});
 
 notificationSchema.post("save", async (doc) => {
 	//
 });
 
 notificationSchema.post("deleteOne", async (doc) => {
-    //
+	//
 });
 
 const notificationModel = mongoose.model<INotification>("Notification", notificationSchema);
