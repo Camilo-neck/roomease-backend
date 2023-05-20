5eb4e8c1092c742fd55c3d3b2a5de4f7d9e93048
Sebastián Piñerez <sebas01pq@gmail.com>
2023-05-20 09:39:59 -0500

    Updated Password to be optional on updateUserSchema

--- src/utils/RouteSchemas/userRouteSchemas.ts
+++ src/utils/RouteSchemas/userRouteSchemas.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 const updateUserSchema: Joi.ObjectSchema = Joi.object({
 	name: Joi.string().required(),
-	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
+	password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).optional(),
 	birth_date: Joi.date().required(),
 	phone: Joi.string().required(),
 	description: Joi.string().required(),
