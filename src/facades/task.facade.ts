5218ab13cd3212737cfb83a3e24435fc70cf7565
Jaime <jmaciass@unal.edu.co>
2023-05-21 02:00:53 -0500

    .

--- src/facades/task.facade.ts
+++ src/facades/task.facade.ts
@@ -192,14 +192,13 @@ async function validate_task(task_id: string, user_id: string): Promise<any> {
 
 const get_week_tasks = (tasks: Document<ITask>[]): Document<ITask>[] => {
 	const today: Date = new Date();
-	const firstDayOfWeek: Date = new Date(today);
-	firstDayOfWeek.setDate(today.getDate() - today.getDay());
-  
+	const firstDayOfWeek: Date = new Date(today.setDate(today.getDate() - today.getDay()));
+
 	return tasks.filter((task: any) => {
-	  const t_date: Date = task.until_date ? task.until_date : task.end_date;
-	  const taskDate: Date = new Date(t_date);
-	  return taskDate >= firstDayOfWeek;
+		const t_date: Date = task.until_date ? task.until_date : task.end_date;
+		const taskDate: Date = new Date(t_date);
+		return taskDate >= firstDayOfWeek;
 	});
-  };
+};
 
 export default new TaskFacade();
