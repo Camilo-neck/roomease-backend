216f149f0cef6ba7b994ce671cdc8a9821eba599
Jaime <jmaciass@unal.edu.co>
2023-05-21 18:09:50 -0500

    mm

--- src/db/models/user.model.ts
+++ src/db/models/user.model.ts
@@ -24,7 +24,7 @@ const userSchema = new Schema(
 
 userSchema.post("deleteOne", async (doc) => {
 	console.log("HOLA");
-	
+
 	await houseModel.updateMany({ $pull: { users: doc._id } });
 	await taskModel.updateMany({ $pull: { users_id: doc._id } });
 });
