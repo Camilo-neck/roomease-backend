baf28ebc9f0c7d3a5d47914794edaa654019c6fa
Camilo-neck <ccuello@unal.edu.co>
2023-05-27 11:13:50 -0500

    Restore Pusher server

--- src/utils/pusher.ts
+++ src/utils/pusher.ts
@@ -1,6 +1,6 @@
-import Pusher from "pusher";
+import PusherServer from "pusher";
 
-export const pusher = new Pusher({
+export const pusher = new PusherServer({
 	appId: process.env.PUSHER_APP_ID ?? "",
 	key: process.env.PUSHER_APP_KEY ?? "",
 	secret: process.env.PUSHER_APP_SECRET ?? "",
