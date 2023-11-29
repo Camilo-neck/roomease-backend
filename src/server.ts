36e4d964a5276986ff2c55c271d1b1018e1c194c
jorozcove <jorozcove@unal.edu.co>
2023-11-28 20:08:38 -0500

    .

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import 'dotenv/config'
-import express from 'express'
+import "dotenv/config";
 
-const port = process.env.PORT || 5001
-const app = express()
+import express from "express";
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+const port = process.env.PORT || 5001;
+const app = express();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
 app.listen(port, () => {
-	console.log(`Server listening on  http://localhost:${port}`)
-})
+	console.log(`Server listening on  http://localhost:${port}`);
+});
